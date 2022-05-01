31b8dbae7fd8d32699f7276bb113bc5712a73064
Ubuntu <ubuntu@ip-172-31-8-119.eu-central-1.compute.internal>
2022-05-01 22:50:57 +0000

    more config setting

--- runDocker/runexperiment.sh
+++ runDocker/runexperiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trap "docker stack rm kauriservice" EXIT
+trap "docker stack rm libhotstuff" EXIT
 
 FILENAME=kauri.yaml
 EXPORT_FILENAME=kauri-temp.yaml
@@ -27,26 +27,26 @@ do
 
   # baseline without failing nodes
   # Deploy experiment
-  docker stack deploy -c kauri-temp.yaml kauriservice &
+  docker stack deploy -c kauri-temp.yaml libhotstuff &
   # Docker startup time + 5*60s of experiment runtime
-  sleep 450
+  sleep 120
 
   echo "******************** Baseline results ******************** "
   # Collect and print results.
   for container in $(docker ps -q -f name="server")
   do
-          if [ ! $(docker exec -it $container bash -c "cd Kauri-Public && test -e log0") ]
+          if [ ! $(docker exec -it $container bash -c "cd libhotstuff && test -e log0") ]
           then
 
-            docker exec -it $container bash -c "cd Kauri-Public && tac log* | grep -m1 'commit <block'"
-            docker exec -it $container bash -c "cd Kauri-Public && tac log* | grep -m1 'x now state'"
-            docker exec -it $container bash -c "cd Kauri-Public && tac log* | grep -m1 'Average'"
+            docker exec -it $container bash -c "cd libhotstuff && tac log* | grep -m1 'commit <block'"
+            docker exec -it $container bash -c "cd libhotstuff && tac log* | grep -m1 'x now state'"
+            docker exec -it $container bash -c "cd libhotstuff && tac log* | grep -m1 'Average'"
+            docker exec -it $container bash -c "cd libhotstuff && tac log* "
+            docker exec -it $container bash -c "cd libhotstuff && cat tlskeys.txt"
           fi
   done
   echo "******************** End of Baseline results ******************** "
-
-  docker stack rm kauriservice
-  sleep 30
+  docker stack rm libhotstuff
 #
 #  for i in {1..2}
 #  do
--- runDocker/server.sh
+++ runDocker/server.sh
@@ -41,7 +41,7 @@ do
   ((i++))
 done
 
-sleep 20
+#sleep 20
 
 # Store all services in the list of IPs (first internal nodes then the leaf nodes)
 dig A $service +short | sort -u | sed -e 's/$/ 1/' >> ips
@@ -49,7 +49,7 @@ dig A $service +short | sort -u | sed -e 's/$/ 1/' >> ips
 sleep 5
 
 # Generate the HotStuff config file based on the given parameters
-python3 scripts/gen_conf.py
+python3 scripts/gen_conf.py --ips "ips"
 
 sleep 20
 
@@ -58,19 +58,19 @@ echo "Starting Application: #${i}"
 # Startup Kauri
 gdb -ex r -ex bt -ex q --args ./examples/hotstuff-app --conf ./hotstuff.gen-sec${id}.conf > log${id} 2>&1 &
 
-sleep 40
+sleep 20
 
 #Configure Network restrictions
 sudo tc qdisc add dev eth0 root netem delay ${latency}ms limit 400000 rate ${bandwidth}mbit &
 
-sleep 25
+sleep 5
 
 # Start Client on Host Machine
 if [ ${id} == 0 ]; then
   gdb -ex r -ex bt -ex q --args ./examples/hotstuff-client --idx ${id} --iter -900 --max-async 900 > clientlog0 2>&1 &
 fi
 
-sleep 300
+sleep 30
 
 killall hotstuff-client &
 killall hotstuff-app &
