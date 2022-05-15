9ca0cd988dfd0058232a2c46de01945f9a240411
LISH0038 <melodyli710@gmail.com>
2022-05-16 01:45:12 +0200

    fix get_child

--- runDocker/findlog0.sh
+++ runDocker/findlog0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for container in $(docker ps -q -f name="server")
 do
-          docker exec -it $container bash -c "cd libhotstuff && cat log0"
+          docker exec -it $container bash -c "cd libhotstuff && cat log1"
 done
