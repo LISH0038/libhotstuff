3239c8c94398d09168298bab75adba16e38358cd
LISH0038 <melodyli710@gmail.com>
2022-05-16 17:08:55 +0200

    test

--- runDocker/findlog0.sh
+++ runDocker/findlog0.sh
@@ -2,4 +2,5 @@
 for container in $(docker ps -q -f name="server")
 do
           docker exec -it $container bash -c "cd libhotstuff && cat log1"
+          docker exec -it $container bash -c "cd libhotstuff && tac log* | grep -m1 'Average'"
 done
