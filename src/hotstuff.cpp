5df1386839b0207613b6be7942fd61eae825a0d1
LISH0038 <melodyli710@gmail.com>
2022-05-29 21:52:22 +0200

    test

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -343,7 +343,7 @@ void HotStuffBase::on_clock(int) {
     delay = ((double) chunk_size) / max_rate;
 
     clock.del();
-    clock.add(delay);
+    clock.add(0.01);
 }
 
 void HotStuffBase::send_propose(Proposal &prop, int tid)
