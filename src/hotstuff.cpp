6927dcedb982d45ef92c4cde33f08f9f06584ee4
LISH0038 <melodyli710@gmail.com>
2022-05-30 03:05:09 +0200

    test

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -324,9 +324,11 @@ void HotStuffBase::on_clock(int) {
     if (id == pmaker->get_proposer() && sndqueue.empty() ) {
         LOG_INFO("Here2, send_index = [%d]", send_index);
         std::unordered_set<PeerId> acks = ackSet[std::string(curProp)];
-        while (send_index < peers.size() - 1 && acks.find(peers[childlist[send_index++]]) != acks.end());
-        if (send_index < peers.size()) {
-            int tid = childlist[--send_index];
+        while (send_index < peers.size() - 1 && acks.find(peers[childlist[send_index]]) != acks.end()) {
+            send_index++;
+        }
+        if (send_index < peers.size() - 1) {
+            int tid = childlist[send_index++];
             LOG_INFO("[%d] enqueue backup msg to %d ", get_id(), tid);
             send_propose(curProp, tid);
         }
